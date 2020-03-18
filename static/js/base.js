b5110dc6f642ea52994c3af0a5f6fe95cebfb34e
Bert <bert.gollnick@posteo.net>
2020-03-18 09:45:57 +0100

    social login styling adapted

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,22 @@
+/* Google Maps */
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: {lat: -33, lng: 151},
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: ['roadmap', 'terrain']
+      }
+    });
+    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+    var beachMarker = new google.maps.Marker({
+    position: {lat: -33.890, lng: 151.274},
+    map: map,
+    icon: image
+  });
+  }
+
 /* ===== SEARCH AUTOFILL ===========*/
 $(document).ready(function(){ 
     $("#txtSearch").autocomplete({
