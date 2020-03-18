9132a400ac68b22c10cea98876c77b07a91d922f
Bert <bert.gollnick@posteo.net>
2020-03-18 17:45:32 +0100

    Google Maps integrated incl. dummy markers
    open: markers need to be linked to our actual db entries

--- djangox_project/settings.py
+++ djangox_project/settings.py
@@ -19,6 +19,17 @@ def get_env_variable(name):
     except KeyError:
       raise ImproperlyConfigured('Environment variable "%s" not found.' % name)
 
+# required for geodjango to work under windows
+""" if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH'] """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -49,7 +60,7 @@ INSTALLED_APPS = [
 
     'social_django',  # social login
     'sslserver',
-    #'leaflet',
+    'django_google_maps',
 
     # Third-party
     'allauth',
@@ -181,7 +192,9 @@ SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = get_env_variable('SOCIAL_AUTH_LINKEDIN_OAUTH2_
 SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = get_env_variable('SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET')
 SOCIAL_AUTH_FACEBOOK_KEY = get_env_variable('SOCIAL_AUTH_FACEBOOK_KEY')        # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = get_env_variable('SOCIAL_AUTH_FACEBOOK_SECRET')  # App Secret
+GOOGLE_MAPS_API_KEY = get_env_variable('GOOGLE_MAPS_API_KEY')
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
 
 SITE_ID = 1
 
