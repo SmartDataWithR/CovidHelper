2b9e0f3a3466d568832e40daa0a4b9639512542c
Gunnar <tehphilb@gmail.com>
2020-03-30 00:32:04 +0200

    Static Files / some design tiny changes
    Should staticfiles not work we should take http://whitenoise.evans.io/en/stable/ in consideration

--- djangox_project/settings.py
+++ djangox_project/settings.py
@@ -180,6 +180,10 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale')
 ]
 
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
 LANGUAGES = [
     ('en', 'English'),
     ('de', 'German'),
