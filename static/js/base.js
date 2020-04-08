95cca4eaff8a4123444cb608efa16a87c60701e6
Bert <bert.gollnick@posteo.net>
2020-04-08 07:51:58 +0200

    App django-messages added and configured; bugfix on signup
    A communications app is added; The checkbox on signup does not disappear any more.

--- static/js/base.js
+++ static/js/base.js
@@ -7,15 +7,30 @@ $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
 });
 
 /*========== CLOSE MOBILE NAV ON CLICK ==========*/
-$(document).ready(function () { //when document loads completely.
-    $(document).click(function (event) { //click anywhere
-        var clickover = $(event.target); //get the target element where you clicked
-        var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
-        if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
-            $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
-        }
-    });
-});
+// $(document).ready(function () { //when document loads completely.
+//     $(document).click(function (event) { //click anywhere
+//         var clickover = $(event.target); //get the target element where you clicked
+//         var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
+//         if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
+//             $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
+//         }
+//     });
+// });
+/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK a[href^="#"] ==========*/
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('.navbar-toggler').addClass('collapsed');
+    $('#navbarResponsive').removeClass('show');
+
+    setTimeout(function () {
+        $('nav.navbar').removeClass('solid-toggle');
+    }, 300);
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+}); 
+
 
 /*========== MULTI-LEVEL / DOUBLE CLICK DROP DOWN MENU ==========*/
 $(document).ready(function () {
