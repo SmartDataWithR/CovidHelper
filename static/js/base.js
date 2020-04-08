97ba35b1454ef66a93b2955eb65633dc90e00137
Gunnar <tehphilb@gmail.com>
2020-04-09 01:22:34 +0200

    New Login/Sign In design

--- static/js/base.js
+++ static/js/base.js
@@ -1,62 +1,39 @@
+// /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+// $(document).ready(function () { //when document(DOM) loads completely
+//     checkScroll(); //check if page is scrolled
+//     $(window).scroll(checkScroll); //get scroll position of window
+// });
+
+// function checkScroll() { //check if page is scrolled
+// if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+//     $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
+// } else { //if page is not scrolled 300px from top
+//     $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
+// }
+// }
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
 $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
     if ($(window).scrollTop() <= 300) { //if window scrolled 300px or less from top
-    $("nav.navbar").toggleClass("solid-toggle"); //add the solid class to navbar
+        $("nav.navbar").toggleClass("solid-toggle"); //add the solid class to navbar
     }
 });
 
-/*========== CLOSE MOBILE NAV ON CLICK ==========*/
-$(document).ready(function () { //when document loads completely.
-    $(document).click(function (event) { //click anywhere
-        var clickover = $(event.target); //get the target element where you clicked
-        var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
-        if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
-            $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
-        }
-    });
+
+/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK a[href^="#"] ==========*/
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('.navbar-toggler').addClass('collapsed');
+    $('#navbarResponsive').removeClass('show');
+
+    setTimeout(function () {
+        $('nav.navbar').removeClass('solid-toggle');
+    }, 300);
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
 });
-
-/*========== MULTI-LEVEL / DOUBLE CLICK DROP DOWN MENU ==========*/
-$(document).ready(function () {
-    var DELAY = 700, clicks = 0, timer = null;
-
-    // On click or double click
-    $("nav ul li.dropdown a.dropdown-toggle")
-        .on("click", function (e) {
-            clicks++;
-            if (clicks === 1) {
-                timer = setTimeout(function () {
-                    clicks = 0;
-                }, DELAY);
-            } else {
-                clearTimeout(timer);
-                window.location.href = $(this).attr('href');
-                clicks = 0;
-            }
-        })
-        .on("dblclick", function (e) {
-            e.preventDefault();
-        });
-
-    //mulit-level menu
-    $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        $(this).siblings().toggleClass("show");
-
-
-        if (!$(this).next().hasClass('show')) {
-            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-        }
-        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
-            $('.dropdown-submenu .show').removeClass("show");
-        });
-
-    });
-});
-
 
 /*========== BOUNCING DOWN ARROW ==========*/
 $(document).ready(function () {
@@ -75,8 +52,43 @@ $(document).ready(function () {
     });
 });
 
+
+/*========== MEET THE TEAM CAROUSEL ==========*/
+$(document).ready(function () { //when document(DOM) loads completely
+    $('#seller-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive: { //set number of items shown per screen width
+            0: {
+                items: 1 //0px width and up display 1 item
+            },
+            768: {
+                items: 2 //768px width and up display 2 items
+            },
+            992: {
+                items: 4 //992px width and up display 3 items
+            }
+        }
+    });
+});
+
+
+/*========== SKILLS COUNTER ==========*/
+$(document).ready(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000,
+        beginAt: 0
+    });
+});
+
+
 /*========== TEAM CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
+$(document).ready(function () { //when document(DOM) loads completely
     $('#team-carousel').owlCarousel({ //owlCarousel settings
         autoplay: true, //set to false to turn off autoplay and only use nav
         autoplayHoverPause: true, //set to false to prevent pausing on hover
@@ -84,78 +96,134 @@ $(document).ready(function(){ //when document(DOM) loads completely
         autoplayTimeout: 8000, //time between transitions
         smartSpeed: 1600, //transition speed
         dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
+        responsive: { //set number of items shown per screen width
+            0: {
                 items: 1 //0px width and up display 1 item
             },
-            768 : {
+            768: {
                 items: 2 //768px width and up display 2 items
             }
         }
     });
-  });
+});
 
 /*========== TOP SCROLL BUTTON ==========*/
-$(document).ready(function () { //when document(DOM) loads completely
-    $(window).scroll(function () { //when webpage is scrolled
-        if ($(this).scrollTop() > 500) { //if scroll from top is more than 500
-            $('.top-scroll').fadeIn(); //display element with class 'top-scroll'
-        } else { //if not
-            $('.top-scroll').fadeOut(); //hide element under 500 px
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.top-scroll').fadeIn();
+        } else {
+            $('.top-scroll').fadeOut();
         }
     });
 });
 
-/*========== WAYPOINTS ANIMATION DELAY ==========*/
-//Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
-$(function () { // a self calling function
-  function onScrollInit(items, trigger) { // a custom made function
-      items.each(function () { //for every element in items run function
-          var osElement = $(this), //set osElement to the current
-              osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
-              osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
 
-          osElement.css({ //change css of element
-              '-webkit-animation-delay': osAnimationDelay, //for safari browsers
-              '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
-              'animation-delay': osAnimationDelay //normal
-          });
-
-          var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
-
-          osTrigger.waypoint(function () { //scroll upwards and downwards
-              osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
-          }, {
-                  triggerOnce: true, //only once this animation should happen
-                  offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
-              });
-      });
-  }
-
-  onScrollInit($('.os-animation')); //function call with only items
-  onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE ==========*/
+$(document).ready(function () {
+    if ($(window).width() < 768) {
+        $('div').attr('data-animation', 'fadeInUp');
+    }
 });
 
 
 
-/*========== Profile ==========*/
-$(document).ready(function() {
+/*========== WAYPOINTS ANIMATION DELAY ==========*/
+//Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
+$(function () { // a self calling function
+    function onScrollInit(items, trigger) { // a custom made function
+        items.each(function () { //for every element in items run function
+            var osElement = $(this), //set osElement to the current
+                osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
+                osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
 
-    
-    var readURL = function(input) {
+            osElement.css({ //change css of element
+                '-webkit-animation-delay': osAnimationDelay, //for safari browsers
+                '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
+                'animation-delay': osAnimationDelay //normal
+            });
+
+            var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+
+            osTrigger.waypoint(function () { //scroll upwards and downwards
+                osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
+            }, {
+                triggerOnce: true, //only once this animation should happen
+                offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
+            });
+        });
+    }
+
+    onScrollInit($('.os-animation')); //function call with only items
+    onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+});
+
+
+/*========== CONTACT FORM INPUT VALIDATION ==========*/
+//Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    $('#contact-form').validator();
+
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "contact/contact.php";
+
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    // data = JSON object that contact.php returns
+
+                    // we recieve the type of the message: success x danger and apply it to the
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
+
+/*========== Profile ==========*/
+$(document).ready(function () {
+
+
+    var readURL = function (input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
                 $('.avatar').attr('src', e.target.result);
             }
-    
+
             reader.readAsDataURL(input.files[0]);
         }
     }
-    
 
-    $(".file-upload").on('change', function(){
+
+    $(".file-upload").on('change', function () {
         readURL(this);
     });
 });
@@ -164,12 +232,7 @@ $(document).ready(function() {
 $(document).ready(function () {
     $("#table").DataTable({
         scrollY: "50vh",
-        "scrollCollapse": true,
-        "searching": false,
-        "paging": false,
-        "info": false,
-        "dom": '<"top"i>rt<"bottom"flp><"clear">'
+        scrollCollapse: true,
+        paging: false
     });
-}); 
-
-
+});
